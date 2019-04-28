65ba3df3973c039333a81d623decbd424c2c3a5a
conqsolid <conqsolid@gmail.com>
2019-04-28 16:01:34 +0300

    babal added and used in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,21 @@ module.exports = {
     entry: __dirname + "/app/src/page.js",
     output: {
         path: __dirname + "/dist/"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: ["/node_modules/"],
+                use: [
+                    {
+                        loader: "babel-loader",
+                        options: {
+                            presets: ["env"]
+                        }
+                    }
+                ]
+            }
+        ]
     }
 }
